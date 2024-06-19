01b2947f30a7ea3bcf52761058ba102d7dd2fb50
SPC <SPC@17-LAB402>
2024-06-19 20:01:52 +0700

    Add project files.

---
+++ CPP_TinhLuyThua.cpp
@@ -0,0 +1,42 @@
+// CPP_TinhLuyThua.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+double TinhLuyThua(double x, double y) {
+    double giatri = 1;
+    for (int i = 0; i < y; i++)
+    {
+        giatri *= x;
+    }
+    cout << "Luy Thua: " << giatri << endl;
+    return giatri;
+}
+
+
+int main()
+{
+    cout << "LuyThua\n";
+    double so, somu, LThua = 1;
+    cout << "Nhap so:"; cin >> so;
+    cout << "Nhap mu:"; cin >> somu;
+    for (int i = 0; i < somu; i++)
+    {
+        LThua *= so;
+    }
+    cout << "Luy Thua: " << LThua << endl;
+    cout << "Luy Thua: " << TinhLuyThua(so, somu) << endl;
+    cout << "Luy Thua: " << pow(so, somu) << endl;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
